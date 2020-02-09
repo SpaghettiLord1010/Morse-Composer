698fef0b78c181c1c5cbf30b228220e7e165f126
Scrivener07 <ScrivenerMods@gmail.com>
2020-02-09 16:10:03 -0500

    Tweaked MessageData.GetCharText() and formatted spaces to tabs.

--- MorseComposer.Data/Data/MessageData.cs
+++ MorseComposer.Data/Data/MessageData.cs
@@ -7,149 +7,152 @@ using System.Threading;
 namespace MorseComposer.Data
 {
 	[TypeConverter(typeof(ExpandableObjectConverter))]
-    public class MessageData
-    {
-        public string Text { get; private set; }
-        public string Translated { get; private set; }
+	public class MessageData
+	{
+		public string Text { get; private set; }
+		public string Translated { get; private set; }
 
-        public List<SymbolData> Symbols { get; private set; }
-
-
-        public MessageData()
-        {
-            Text = string.Empty;
-            Translated = string.Empty;
-            Symbols = new List<SymbolData>();
-        }
+		public List<SymbolData> Symbols { get; private set; }
 
 
-        public bool Submit(string text)
-        {
-            Text = text;
-            Symbols.Clear();
-
-            foreach (char character in text)
-            {
-                if (char.IsLetterOrDigit(character) || character == ' ')
-                {
-                    SymbolData symbol = new SymbolData();
-                    symbol.Character = character;
-                    Symbols.Add(symbol);
-                }
-                else
-                {
-                    Trace.WriteLine("The character \"" + character + "\" is not a letter, skipping.");
-                }
-            }
-
-            return Symbols.Count > 0;
-        }
+		public MessageData()
+		{
+			Text = string.Empty;
+			Translated = string.Empty;
+			Symbols = new List<SymbolData>();
+		}
 
 
-        public bool Translate()
-        {
-            Translated = string.Empty;
+		public bool Submit(string text)
+		{
+			Text = text;
+			Symbols.Clear();
 
-            if (string.IsNullOrWhiteSpace(Text))
-            {
-                Trace.WriteLine("There is no text to translate.");
-                return false;
-            }
-            else if (Symbols.Count < 1)
-            {
-                Trace.WriteLine("There are no symbols to translate.");
-                return false;
-            }
-            else
-            {
-                foreach (var symbol in Symbols)
-                {
-                    if (symbol.Character == ' ')
-                    {
-                        Translated += Lexicon.WordSpace;
-                    }
-                    else
-                    {
-                        if (char.IsLetterOrDigit(symbol.Character))
-                        {
-                            bool found = Lexicon.Alphabet.TryGetValue(symbol.Character, out string code);
+			foreach (char character in text)
+			{
+				if (char.IsLetterOrDigit(character) || character == ' ')
+				{
+					SymbolData symbol = new SymbolData();
+					symbol.Character = character;
+					Symbols.Add(symbol);
+				}
+				else
+				{
+					Trace.WriteLine("The character \"" + character + "\" is not a letter, skipping.");
+				}
+			}
 
-                            if (found)
-                            {
-                                symbol.Code = code;
+			return Symbols.Count > 0;
+		}
 
 
-                                if (!string.IsNullOrEmpty(Translated))
-                                {
-                                    Translated += Lexicon.LetterSpace;
-                                }
-                                Translated += code;
-                            }
-                            else
-                            {
-                                throw new Exception("The character \"" + symbol.Character + "\" could not be found.");
-                            }
-                        }
-                        else
-                        {
-                            Trace.WriteLine("The character \"" + symbol.Character + "\" is not a letter.");
-                        }
-                    }
-                }
+		public bool Translate()
+		{
+			Translated = string.Empty;
 
-                return true;
-            }
-        }
+			if (string.IsNullOrWhiteSpace(Text))
+			{
+				Trace.WriteLine("There is no text to translate.");
+				return false;
+			}
+			else if (Symbols.Count < 1)
+			{
+				Trace.WriteLine("There are no symbols to translate.");
+				return false;
+			}
+			else
+			{
+				foreach (var symbol in Symbols)
+				{
+					if (symbol.Character == ' ')
+					{
+						Translated += Lexicon.WordSpace;
+					}
+					else
+					{
+						if (char.IsLetterOrDigit(symbol.Character))
+						{
+							bool found = Lexicon.Alphabet.TryGetValue(symbol.Character, out string code);
 
-
-        public bool Play()
-        {
-            if (Symbols.Count > 0)
-            {
-                const int duration = 500;
-                foreach (var symbol in Symbols)
-                {
-                    Console.Beep(symbol.Tone, duration);
-                    Thread.Sleep(Convert.ToInt32(symbol.Delay));
-                    Trace.WriteLine(string.Format("BEEP! (Tone:{0}, Duration:{1}, Delay:{2})", symbol.Tone, duration, symbol.Delay));
-                }
-                return true;
-            }
-            else
-            {
-                Trace.WriteLine("There are no symbols to play tones.");
-                return false;
-            }
-        }
-
-        public string GetCharText()
-        {
-            int tone = 0;
-            int time = 0;
-            int duration = 0;
-            string finishedText = "";
-
-            foreach (SymbolData character in Symbols)
-            {
-                //setting the lenght
-                if(character.ToString() == ".")
-                {
-                    duration = 1;
-                }
-                else if (character.ToString() == "-")
-                {
-                    duration = 2;
-                }
-                //setting the frequency
-                tone = character.Tone;
-                finishedText = finishedText + Environment.NewLine + tone + Environment.NewLine + time + Environment.NewLine + duration;
+							if (found)
+							{
+								symbol.Code = code;
 
 
-                
-            }
-            return finishedText;
-        }
+								if (!string.IsNullOrEmpty(Translated))
+								{
+									Translated += Lexicon.LetterSpace;
+								}
+								Translated += code;
+							}
+							else
+							{
+								throw new Exception("The character \"" + symbol.Character + "\" could not be found.");
+							}
+						}
+						else
+						{
+							Trace.WriteLine("The character \"" + symbol.Character + "\" is not a letter.");
+						}
+					}
+				}
+
+				return true;
+			}
+		}
 
 
-    }
+		// TODO: Needs to handle whitespace delays correctly.
+		public bool Play()
+		{
+			if (Symbols.Count > 0)
+			{
+				const int duration = 500;
+				foreach (var symbol in Symbols)
+				{
+					Console.Beep(symbol.Tone, duration);
+					Thread.Sleep(Convert.ToInt32(symbol.Delay));
+					Trace.WriteLine(string.Format("BEEP! (Tone:{0}, Duration:{1}, Delay:{2})", symbol.Tone, duration, symbol.Delay));
+				}
+				return true;
+			}
+			else
+			{
+				Trace.WriteLine("There are no symbols to play tones.");
+				return false;
+			}
+		}
+
+
+		// TODO: The `SymbolData.Character` is NOT a dot or dash. Its the alphabetical letter.
+		// The comparison below is incorrect.
+		public string GetCharText()
+		{
+			const int time = 0; // TODO: This is an unused variable!
+			int duration = 0;
+			string text = string.Empty;
+
+			foreach (var symbol in Symbols)
+			{
+				// setting the length
+				if (symbol.Character == '.')
+				{
+					duration = 1;
+				}
+				else if (symbol.Character == '-')
+				{
+					duration = 2;
+				}
+				else
+				{
+					// unhandled code branch
+				}
+				// setting the frequency
+				text += Environment.NewLine + symbol.Tone + Environment.NewLine + time + Environment.NewLine + duration;
+			}
+			return text;
+		}
+
+
+	}
 }
--- MorseComposer.Data/Data/SymbolData.cs
+++ MorseComposer.Data/Data/SymbolData.cs
@@ -4,43 +4,46 @@ using System.Linq;
 namespace MorseComposer.Data
 {
 	[TypeConverter(typeof(ExpandableObjectConverter))]
-    public class SymbolData
-    {
-        /// <summary>
-        /// The letter character for this symbol.
-        /// </summary>
-        public char Character { get; set; }
+	public class SymbolData
+	{
+		/// <summary>
+		/// The letter character for this symbol.
+		/// </summary>
+		/// <example>A, B, C, & D</example>
+		public char Character { get; set; }
 
-        /// <summary>
-        /// The Morse code character for this symbol.
-        /// </summary>
-        public string Code { get; set; }
+		/// <summary>
+		/// The Morse code character for this symbol.
+		/// </summary>
+		/// <example>....</example>
+		/// <example>----</example>
+		public string Code { get; set; }
 
-        /// <summary>
-        /// The tone frequency for this symbol.
-        /// </summary>
-        public int Tone { get; set; }
+		/// <summary>
+		/// The tone frequency for this symbol.
+		/// </summary>
+		public int Tone { get; set; }
 
-        /// <summary>
-        /// The tone delay for this symbol.
-        /// </summary>
-        public double Delay { get; set; }
-
-
-        public SymbolData()
-        {
-            Character = Lexicon.Alphabet.Keys.ToArray()[0]; ;
-            Code = string.Empty;
-            Tone = Lexicon.Frequencies.Values.ToArray()[0].Frequency;
-            Delay = 1.0;
-        }
+		/// <summary>
+		/// The tone delay for this symbol.
+		/// </summary>
+		public double Delay { get; set; }
 
 
-        public override string ToString()
-        {
-            return Character.ToString();
-        }
+		public SymbolData()
+		{
+			Character = Lexicon.Alphabet.Keys.ToArray()[0]; ;
+			Code = string.Empty;
+			Tone = Lexicon.Frequencies.Values.ToArray()[0].Frequency;
+			Delay = 1.0;
+		}
 
 
-    }
+		public override string ToString()
+		{
+			return Character.ToString();
+		}
+
+
+	}
 }
